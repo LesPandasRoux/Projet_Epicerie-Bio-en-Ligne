3d17fc2b0e97a548a4db81c1ab265716654e5d0c
Manoone <38104491+Manoone@users.noreply.github.com>
2018-04-12 17:03:58 +0200

    Update Commande.java

--- src/main/java/com/fr/adaming/model/Commande.java
+++ src/main/java/com/fr/adaming/model/Commande.java
@@ -1,17 +1,17 @@
 package com.fr.adaming.model;
 
-import java.util.Date;
 import java.util.List;
 
+
+
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
+
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 
 @Entity
 public class Commande {
@@ -20,22 +20,30 @@ public class Commande {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long refCMD;
 
-	@ManyToOne(fetch=FetchType.LAZY)
+	@ManyToOne
 	@JoinColumn(name = "OWNER_ID")
 	private User owner;
 
-	private Date dateCMD;
+	private String dateCMD;
 	private String etatCommande;
 
+	@OneToOne
+	@JoinColumn(name="fk_panier",referencedColumnName="idPanier")
+	private Panier panier;
+	
+	
 
-	@ManyToMany(fetch=FetchType.LAZY)
-	@JoinTable(
-			name = "CMD_PRODUCT", 
-			joinColumns = @JoinColumn(name = "CMD_ID", referencedColumnName = "refCMD"), 
-			inverseJoinColumns = @JoinColumn(name = "PRODUCT_ID", referencedColumnName = "ID"))
-	private List<Produit> produits;
-	
-	
+	public Commande() {
+		super();
+	}
+
+	public Commande(User owner, String dateCMD, String etatCommande, Panier panier) {
+		super();
+		this.owner = owner;
+		this.dateCMD = dateCMD;
+		this.etatCommande = etatCommande;
+		this.panier = panier;
+	}
 
 	public Long getRefCMD() {
 		return refCMD;
@@ -53,21 +61,14 @@ public class Commande {
 		this.owner = owner;
 	}
 
-	public Date getDateCMD() {
+	public String getDateCMD() {
 		return dateCMD;
 	}
 
-	public void setDateCMD(Date dateCMD) {
+	public void setDateCMD(String dateCMD) {
 		this.dateCMD = dateCMD;
 	}
 
-	public List<Produit> getProduits() {
-		return produits;
-	}
-
-	public void setProduits(List<Produit> produits) {
-		this.produits = produits;
-	}
 
 	public String getEtatCommande() {
 		return etatCommande;
@@ -75,6 +76,14 @@ public class Commande {
 
 	public void setEtatCommande(String etatCommande) {
 		this.etatCommande = etatCommande;
+	}
+
+	public Panier getPanier() {
+		return panier;
+	}
+
+	public void setPanier(Panier panier) {
+		this.panier = panier;
 	}
 	
 	
